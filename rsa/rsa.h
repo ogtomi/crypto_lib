17044305798eceaf8ceec6fbb7bd32e2a48b9e53
ogtomi <tomeklewandowski570@gmail.com>
2023-02-05 13:09:04 +0100

    rsa.h: public/private key struct added, generate keys function added

--- rsa/rsa.h
+++ rsa/rsa.h
@@ -1,30 +1,48 @@
 #ifndef _RSA_H_
 #define _RSA_H_
 
-class RSA
+#include "../math_algorithms/euclidean.h"
+
+struct PublicKey
+{
+    int e;
+    int n;
+};
+
+struct PrivateKey
+{
+    int d;
+    int n;
+};
+
+class RSA : Euclidean
 {
 private:
-    double p;
-    double q;
-    double phi;
-    double n;
-    double e;
-    double d;
-    static constexpr double k = 2;
+    int p;
+    int q;
+    int phi;
+    int n;
+    int e;
+    int d;
+
+    PublicKey* public_key;
+    PrivateKey* private_key;
 
 public:
-    RSA(double p, double q);
+    RSA(int p, int q);
     ~RSA(){};
 
-    double encrypt(double message);
-    double decrypt(double message);
+    int encrypt(int message);
+    int decrypt(int message);
 
 private:
-    void calculate_n(){n = p * q;};
-    void calculate_phi(){phi = (p - 1) * (q - 1);};
+    bool is_prime(int num);
+    void calculate_e();
     void calculate_d();
-    int gcd(int a, int b);
-    void choose_e();
+    void set_params();
+    void gen_public_key();
+    void gen_private_key();
+    void gen_keys();
 };
 
 #endif
