2d52303ef5a4aa81a12fa6bbc3ba7032a9416ad6
ogtomi <tomeklewandowski570@gmail.com>
2023-02-05 11:52:06 +0100

    euclidean.cpp: Added extended euclidean function implementation

--- math_algorithms/euclidean.cpp
+++ math_algorithms/euclidean.cpp
@@ -7,3 +7,21 @@ int Euclidean::euclidean(int a, int b)
     
     return euclidean(b, a % b);
 }
+
+int Euclidean::extended_euclidean(int a, int b, int &x, int &y)
+{
+    if(b == 0)
+    {
+        x = 1;
+        y = 0;
+
+        return a;
+    }
+
+    int x1, y1;
+    int gcd = extended_euclidean(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - (a / b) * y1;
+    
+    return gcd;
+}
