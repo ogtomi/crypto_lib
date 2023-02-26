d7223c4669e5cfa5b58f6ec14366d59e15f8d542
ogtomi <tomeklewandowski570@gmail.com>
2023-02-26 10:23:49 +0100

    main.cpp: Run DES

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
+#include <string>
 #include "rsa/rsa.h"
+#include "sha256/sha256.h"
+#include "des/des.h"
 
 int main()
 {
-    RSA rsa;
+    /*RSA rsa;
     rsa.gen_keys();
 
-    mpz_class message = 120;
+    mpz_class message = 956;
     mpz_class encrypted_message;
     mpz_class decrypted_message;
 
@@ -17,6 +20,19 @@ int main()
     std::cout << "Plain message: " << message << std::endl;
     std::cout << "Encrypted message: " << encrypted_message << std::endl;
     std::cout << "Decrypted message: " << decrypted_message << std::endl;
+
+    SHA256 sha256;
+    std::string hash_str = sha256.digest("Just trying to make the message longer. Adding some stuff");
+    std::cout << hash_str << std::endl; 
+    */
+    std::string message = "0123456789abcdef";
+    std::string key = "133457799bbcdff1";
+
+    DES des;
+
+    std::cout << message << std::endl;
+    des.run_testing(key, message);
+    std::cout << message << std::endl;
 
     return 0;
 }
