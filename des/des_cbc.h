27b8da09313316065c4836cc257fee6e6a51bab2
ogtomi <tomeklewandowski570@gmail.com>
2023-03-19 13:42:09 +0100

    des_cbc.h: DES CBC mode header initial commit

---
+++ des/des_cbc.h
@@ -0,0 +1,21 @@
+#ifndef _DES_CBC_H_
+#define _DES_CBC_H_
+
+#include "des.h"
+#include <vector>
+
+class DES_CBC: public DES
+{
+private:
+    std::bitset<1> subkeys[16][56];
+    std::bitset<1> perm_subkeys[16][48];
+
+public:
+    void encrypt(std::string &message);
+    void decrypt(std::string &cipher);
+
+private:
+    void split_message(const std::string &message, std::vector<std::string> &message_vec);
+};
+
+#endif
