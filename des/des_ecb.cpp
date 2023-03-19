4e990cf3599e5a9fd041b1b631787a34a289c069
ogtomi <tomeklewandowski570@gmail.com>
2023-03-19 13:17:18 +0100

    des_ecb.cpp: Implementation of the encryption function

--- des/des_ecb.cpp
+++ des/des_ecb.cpp
@@ -4,7 +4,7 @@ void DES_ECB::split_message(const std::string &message, std::vector<std::string>
 {
     int message_block_len = 16;
 
-    for(size_t i = 0; i < message.size(); i+=message_block_len)
+    for(size_t i = 0; i < message.size(); i += message_block_len)
     {
         message_vec.push_back(message.substr(i, message_block_len));
     }
@@ -15,8 +15,15 @@ void DES_ECB::encrypt(std::string &message)
     std::vector<std::string> message_vec;
     split_message(message, message_vec);
 
-    for(const auto &sub_string: message_vec)
+    for(size_t i = 0; i < message_vec.size(); i++)
     {
-        std::cout << sub_string << std::endl;
+        DES::encrypt(message_vec[i]);
+    }
+
+    message = "";
+
+    for(size_t i = 0; i < message_vec.size(); i++)
+    {
+        message += message_vec[i];
     }
 }
