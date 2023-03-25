51c225cc77126b385644e890ad730da228690560
ogtomi <tomeklewandowski570@gmail.com>
2023-03-25 14:07:36 +0100

    aes128.cpp: Implementation of get_state_arr function

--- aes/aes128.cpp
+++ aes/aes128.cpp
@@ -7,14 +7,41 @@ void AES_128::ascii_to_hex(std::string &ascii_str)
     for(size_t i = 0; i < ascii_str.size(); i++)
     {
         ss.width(2);
-        ss << std::hex << (unsigned) ascii_str[i];
+        ss << std::hex << (unsigned int) ascii_str[i];
     }
 
     ascii_str = ss.str();
 }
 
+void AES_128::get_state_arr(const std::string &hex_str, uint8_t state_arr[][4])
+{   
+    std::string byte_str;
+    int byte_num = 0;
+    int word_num = 0;
+
+    for(size_t i = 0; i < hex_str.size(); i+=2)
+    {   
+        byte_str = hex_str.substr(i, 2);
+        state_arr[word_num][byte_num] = std::stoul(byte_str, 0, 16);
+
+        byte_num++;
+
+        if(byte_num == 4)
+        {
+            byte_num = 0;
+            word_num++;
+        }
+    }
+}
+
 void AES_128::run_testing(std::string &input)
 {
     ascii_to_hex(input);
+    get_state_arr(input, state_arr);
+
+    for(const auto &word: state_arr)
+        for(const auto &byte: word)
+            std::cout << std::hex << (unsigned int) byte;
+    std::cout << "\n";
     std::cout << input << std::endl;
 }
