136ebcdf8ef5aa0ecc4e70dfa9880ed63fc4756e
ogtomi <tomeklewandowski570@gmail.com>
2023-04-01 12:03:12 +0200

    aes128.cpp: Changed sub functions to generic (names), implemented rot_word function

--- aes/aes128.cpp
+++ aes/aes128.cpp
@@ -13,7 +13,7 @@ void AES_128::ascii_to_hex(std::string &ascii_str)
     ascii_str = ss.str();
 }
 
-void AES_128::get_state_arr(const std::string &hex_str, uint8_t state_arr[][4])
+void AES_128::get_arr(const std::string &hex_str, uint8_t arr[][4])
 {   
     std::string byte_str;
     int row = 0;
@@ -22,7 +22,7 @@ void AES_128::get_state_arr(const std::string &hex_str, uint8_t state_arr[][4])
     for(size_t i = 0; i < hex_str.size(); i+=2)
     {   
         byte_str = hex_str.substr(i, 2);
-        state_arr[row][col] = std::stoul(byte_str, 0, 16);
+        arr[row][col] = std::stoul(byte_str, 0, 16);
 
         row++;
 
@@ -34,7 +34,7 @@ void AES_128::get_state_arr(const std::string &hex_str, uint8_t state_arr[][4])
     }
 }
 
-void AES_128::sub_bytes(uint8_t state_arr[][4])
+void AES_128::sub_bytes(uint8_t arr[][4])
 {
     uint8_t row_value = 0;
     uint8_t col_value = 0;
@@ -43,27 +43,52 @@ void AES_128::sub_bytes(uint8_t state_arr[][4])
     {
         for(int row = 0; row < 4; row++)
         {
-            row_value = (state_arr[row][col] >> 4) & 0x0F;
-            col_value = state_arr[row][col] & 0x0F;
-            state_arr[row][col] = s_box[row_value][col_value];
+            row_value = (arr[row][col] >> 4) & 0x0F;
+            col_value = arr[row][col] & 0x0F;
+            arr[row][col] = s_box[row_value][col_value];
         }
     }
 }
 
+void AES_128::rot_word(uint8_t *word)
+{
+    uint8_t temp = word[0];
+
+    for(int i = 0; i < 3; i++)
+    {
+        word[i] = word[i + 1];
+    }
+
+    word[3] = temp;
+}
+
+void AES_128::add_round_key(uint8_t key_arr[][4])
+{
+
+}
+
+
+
 void AES_128::run_testing(std::string &key, std::string &message)
 {
     ascii_to_hex(key);
-    get_state_arr(key, state_arr);
-
-    /*(for(auto const &bytes: state_arr)
+    get_arr(key, key_arr);
+    sub_bytes(key_arr);
+    
+    for(const auto &byte: key_arr[0])
     {
-        for(auto const &byte: bytes)
-        {
-            std::cout << std::hex << (unsigned) byte << " ";
-        }
-        std::cout << "\n";
-    }*/
-    sub_bytes(state_arr);
+        std::cout << std::hex << (unsigned) byte;
+    }
+
     std::cout << "\n";
-    std::cout << key << std::endl;
+
+    rot_word(key_arr[0]);
+
+    for(const auto &byte: key_arr[0])
+    {
+        std::cout << std::hex << (unsigned) byte;
+    }
+
+    std::cout << "\n";
+
 }
