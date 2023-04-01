d7de995c63b39bbf0d3f86b955045f88e55cab19
ogtomi <tomeklewandowski570@gmail.com>
2023-04-01 17:28:54 +0200

    aes128.h: Added uint32 to uint8 conversion function

--- aes/aes128.h
+++ aes/aes128.h
@@ -20,6 +20,7 @@ private:
     void ascii_to_hex(std::string &ascii_str);
     void get_arr(const std::string &hex_str, uint8_t arr[][4]);
     void uint8_to_32(const uint8_t *arr, uint32_t &word);
+    void uint32_to_8(uint8_t *arr, const uint32_t &word);
     void sub_bytes(uint8_t arr[][4]);
     void rot_word(uint8_t *byte_arr);
     void sub_word(uint8_t *byte_arr);
