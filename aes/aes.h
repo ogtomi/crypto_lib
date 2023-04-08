30d64e827ca04ecb5ca813487b472c1a3fdb71be
ogtomi <tomeklewandowski570@gmail.com>
2023-04-08 13:35:32 +0200

    aes.h: Deleted conversion functions (8 --> 32 bits)

--- aes/aes.h
+++ aes/aes.h
@@ -27,8 +27,6 @@ private:
     void ascii_to_hex(std::string &ascii_str);
     void get_state_arr(const std::string &hex_str, uint8_t state_arr[][4]);
     void get_key_arr(const std::string &hex_str, uint8_t key_arr[][4]);
-    void uint8_to_32(const uint8_t *arr, uint32_t &word);
-    void uint32_to_8(uint8_t *arr, const uint32_t &word);
     void rot_word(uint8_t *byte_arr);
     void sub_word(uint8_t *byte_arr);
     void expand_key(uint8_t arr[][4], uint8_t key_expanded[][4]);
