3404f3a6134c470b6dde206a30886ed60c22ee5e
ogtomi <tomeklewandowski570@gmail.com>
2023-04-10 12:37:05 +0200

    main.cpp: Added decryption for AES

--- main.cpp
+++ main.cpp
@@ -15,10 +15,12 @@ int main()
     std::string key_256 = "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f";
     std::string message = "00112233445566778899aabbccddeeff";
 
-    AES aes(AES_key_length::AES_128);
-    aes.generate_keys(key_128);
+    AES aes(AES_key_length::AES_256);
+    aes.generate_keys(key_256);
     std::cout << message << std::endl;
     aes.encrypt(message);
+    std::cout << message << std::endl;
+    aes.decrypt(message);
     std::cout << message << std::endl;
 
     return 0;
