0dfd2ae5890e09f55302c6faf30486c10b2fa855
ogtomi <tomeklewandowski570@gmail.com>
2023-04-15 18:27:42 +0200

    main.cpp: Run main for AES-CBC mode

--- main.cpp
+++ main.cpp
@@ -14,14 +14,15 @@ int main()
     std::string key_192 = "000102030405060708090a0b0c0d0e0f1011121314151617";
     std::string key_256 = "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f";
     std::string message = "00112233445566778899aabbccddeeff";
+    std::string message_ecb = "00112233445566778899aabbccddeeff00112233445566778899aabbccddeeff";
 
-    AES aes(AES_key_length::AES_256);
-    aes.generate_keys(key_256);
-    std::cout << message << std::endl;
-    aes.encrypt(message);
-    std::cout << message << std::endl;
-    aes.decrypt(message);
-    std::cout << message << std::endl;
+    AES aes(AES_key_length::AES_128);
+    aes.generate_keys(key_128);
+    std::cout << message_ecb << std::endl;
+    aes.encrypt(message_ecb, AES_mode::CBC);
+    std::cout << message_ecb << std::endl;
+    aes.decrypt(message_ecb, AES_mode::CBC);
+    std::cout << message_ecb << std::endl;
 
     return 0;
 }
