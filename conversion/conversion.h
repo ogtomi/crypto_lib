70199cc6de5523291dae83a5704f542585421d64
ogtomi <tomeklewandowski570@gmail.com>
2023-05-06 12:36:01 +0200

    conversion.h: split message function

--- conversion/conversion.h
+++ conversion/conversion.h
@@ -1,7 +1,9 @@
 #include <string>
 #include <iomanip>
 #include <sstream>
+#include <vector>
 
 void hexstr_to_uint8t(const std::string &hex_str, uint8_t *data);
 void uint8t_to_hexstr(std::string &hex_str, uint8_t *data, const size_t data_len);
 void ascii_to_hex(std::string &ascii_str);
+void split_message(const std::string &message, std::vector<std::string> &message_vec);
