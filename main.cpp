ee9a626f4ad46f2a18cdfc6f29168258313e19c6
ogtomi <tomeklewandowski570@gmail.com>
2023-05-07 13:45:35 +0200

    main.cpp: Aligned variable names for messages and encryption type

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include "rsa/rsa.h"
 #include "sha256/sha256.h"
 #include "des/des.h"
 #include "des/des_ecb.h"
@@ -19,18 +18,18 @@ int main()
     std::string key_192 = "000102030405060708090a0b0c0d0e0f1011121314151617";
     std::string key_256 = "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f";
     std::string message = "00112233445566778899aabbccddeeff";
-    std::string message_ecb = "00112233445566778899aabbccddeeff00112233445566778899aabbccddeeff";
+    std::string message_ll = "00112233445566778899aabbccddeeff00112233445566778899aabbccddeeff";
 
     AES aes(AES_key_length::AES_128);
     aes.generate_keys(key_128);
-    CTR<AES> aes_cbc(aes);
+    CBC<AES> aes_cbc(aes);
     
     std::cout << "META" << std::endl;
-    std::cout << message_ecb << std::endl;
-    aes_cbc.encrypt(message_ecb);
-    std::cout << message_ecb << std::endl;
-    aes_cbc.decrypt(message_ecb);
-    std::cout << message_ecb << std::endl;
+    std::cout << message_ll << std::endl;
+    aes_cbc.encrypt(message_ll);
+    std::cout << message_ll << std::endl;
+    aes_cbc.decrypt(message_ll);
+    std::cout << message_ll << std::endl;
 
     return 0;
 }
