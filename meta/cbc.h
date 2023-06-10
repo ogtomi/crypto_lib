5f417ed9940fb8d41d0d040e53d233dc0ba066ca
ogtomi <tomeklewandowski570@gmail.com>
2023-06-10 13:21:20 +0200

    cbc.h: Added block size

--- meta/cbc.h
+++ meta/cbc.h
@@ -17,22 +17,22 @@ private:
     std::string iv;
 
 public:
-    CBC(AlgorithmType& alg)
-        :alg(alg)
+    CBC(AlgorithmType& alg, const uint8_t &iv_len)
+        :alg(alg), iv_len(iv_len)
     {
         iv = rng.generate_iv(iv_len);
     };
 
-    void encrypt(std::string &message)
+    void encrypt(std::string &message, const int &block_size)
     {
         std::string init_vec = iv;
         std::vector<std::string> message_vec;
 
-        split_message(message, message_vec);
+        split_message(message, message_vec, block_size * 2);
 
         for(size_t i = 0; i < message_vec.size(); i++)
         {
-            xor_iv(message_vec[i], init_vec);
+            xor_iv(message_vec[i], init_vec, block_size);
             alg.encrypt(message_vec[i]);
             init_vec = message_vec[i];
         }
@@ -45,13 +45,13 @@ public:
         } 
     };
 
-    void decrypt(std::string &cipher)
+    void decrypt(std::string &cipher, const int &block_size)
     {
         std::string init_vec = iv;
         std::vector<std::string> cipher_vec;
         std::string temp_vec{};
 
-        split_message(cipher, cipher_vec);
+        split_message(cipher, cipher_vec, block_size * 2);
 
         for(size_t i = 0; i < cipher_vec.size(); i++)
         {
@@ -62,7 +62,7 @@ public:
 
             temp_vec = cipher_vec[i];
             alg.decrypt(cipher_vec[i]);
-            xor_iv(cipher_vec[i], init_vec);
+            xor_iv(cipher_vec[i], init_vec, block_size);
         }
 
         cipher = "";
