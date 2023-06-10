dd8dfa8a4ecda0c5c66060da000cdbb040a5f3bb
ogtomi <tomeklewandowski570@gmail.com>
2023-06-10 12:30:46 +0200

    conversion.h: Added block_size parameter for split message

--- conversion/conversion.h
+++ conversion/conversion.h
@@ -6,4 +6,4 @@
 void hexstr_to_uint8t(const std::string &hex_str, uint8_t *data);
 void uint8t_to_hexstr(std::string &hex_str, uint8_t *data, const size_t data_len);
 void ascii_to_hex(std::string &ascii_str);
-void split_message(const std::string &message, std::vector<std::string> &message_vec);
+void split_message(const std::string &message, std::vector<std::string> &message_vec, const int &block_size);
